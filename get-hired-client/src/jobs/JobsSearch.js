5b4c0af92f6516de82b8bc3cf23dbe5b1c0b4c69
EdenHamami <edenha1900@gmail.com>
2023-05-14 16:20:59 +0200

    Fill in 2 fields - job search

--- get-hired-client/src/jobs/JobsSearch.js
+++ get-hired-client/src/jobs/JobsSearch.js
@@ -6,33 +6,31 @@ import jobsList from './JobsList'
 import Job from './Job'
 import './JobsSearch.css';
 
-
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>import './Text_Field.css';
-  <script src="~/lib/signalr/signalr.js"></script></link>
 function JobsSearch() {
-
   let navigate = useNavigate();
   const [jobdescription, setjobdescription] = useState('');
   const [joblocation, setjoblocation] = useState('');
   const [resultList, setResultList] = useState([]);
+  const [error, setError] = useState('');
 
   const Jobs = resultList.map((con, key) => {
     return <Job {...con} key={key} />
   });
  
   async function handleSearch(event) {
-    event.preventDefault();   
+    event.preventDefault(); 
+    if(!jobdescription || !joblocation) {
+      setError('Both fields are required');
+      return;
+    }  
     navigate('/JobsSearchResult', { state: {jobdescription: jobdescription, joblocation: joblocation} });
   }
+  
   const handleOnSelectJob = (item) => {
-    // the item selected
     setjobdescription(item.name)
   }
+  
   const handleOnSelectCity = (item) => {
-    // the item selected
     setjoblocation(item.name)
   }
 
@@ -56,7 +54,7 @@ function JobsSearch() {
             autoFocus
           />
           <br />
-    
+          {error && <p>{error}</p>}
         </div>
         <div className='findJobs'>
           <button type="button" onClick={handleSearch} className="findJobs-search-button">Find Jobs</button>
@@ -65,4 +63,5 @@ function JobsSearch() {
     </div>
   )
 }
-export default JobsSearch
+
+export default JobsSearch;
