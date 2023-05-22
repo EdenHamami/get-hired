6df26747d49af8faab92dadf5177e66ff71031ed
EdenHamami <edenha1900@gmail.com>
2023-05-22 21:31:29 +0200

    education

--- get-hired-client/src/SharedComponents/EducationList.js
+++ get-hired-client/src/SharedComponents/EducationList.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './EducationList.css';
 
-const EducationList = ({ education, template,header }) => {
+const EducationList = ({ education, template, header }) => {
   return (
     <div className={`education-${template}`}>
       <h2>{header}</h2>
       {education.map((edu, index) => (
         <div key={index} className="education-item">
-          <h3>{edu.degree}, {edu.institution}</h3>
-          <p className='date'> {edu.startDate} - {edu.endDate}</p>
-          <p className='grade'>Grade: {edu.grade}</p>
-          <p className='description'>{edu.description}</p>
+          <h3>
+            {edu.degree}
+            {edu.degree && edu.institution && ', '}
+            {edu.institution}
+          </h3>
+          <p className='date'>
+            {edu.startDate}
+            {edu.startDate && edu.endDate && '-'}
+            {edu.endDate}
+          </p>
+          {edu.grade && <p className='grade'>Grade: {edu.grade}</p>}
+          {edu.description && <p className='description'>{edu.description}</p>}
         </div>
       ))}
     </div>
