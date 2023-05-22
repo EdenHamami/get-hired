5442a120e032e07dfdce705c65fc59409c243044
Orpaz <orpaz234@gmail.com>
2023-05-22 17:01:45 +0300

    without bottom line video

--- get-hired-client/src/VirtualInterview/VideoInterviewer.js
+++ get-hired-client/src/VirtualInterview/VideoInterviewer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
@@ -6,33 +6,47 @@ import 'video.js/dist/video-js.css';
 const VideoInterviewer = (props) => {
   const { width, height, src } = props;
   const videoRef = useRef(null);
+  const [showRestartButton, setShowRestartButton] = useState(false);
 
   useEffect(() => {
     const player = videojs(videoRef.current, {
-      controls: true,
+      controls: false, // Disable controls
       autoplay: true,
       preload: 'auto',
-      poster: 'path/to/poster/image.jpg',
     });
 
-    // player.on('ended', () => {
-    //   player.currentTime(0);
-    // });
+    player.on('ended', () => {
+      setShowRestartButton(true);
+    });
 
     return () => {
       player.dispose();
     };
   }, []);
 
+  const restartVideo = () => {
+    setShowRestartButton(false);
+    const player = videojs(videoRef.current);
+    player.currentTime(0); // Rewind the video to the beginning
+    player.play(); // Start playing again
+  };
+
   const navigate = useNavigate();
 
   return (
     <div>
+      <video
+        ref={videoRef}
+        className="video-js vjs-default-skin"
+        width={width}
+        height={height}
+        src={src}
+        type="video/mp4"
+      />
 
-
-    <video ref={videoRef} className="video-js vjs-default-skin" width={width} height={height} src={src} type="video/mp4" />
-
-
+      {showRestartButton && (
+        <button onClick={restartVideo}>Restart Video</button>
+      )}
     </div>
   );
 };
