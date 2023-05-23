c91b802ca29b037490b8c90622d4f8e8cb9b289b
Orpaz <orpaz234@gmail.com>
2023-05-23 15:48:03 +0300

    page4

--- get-hired-client/src/VirtualInterview/TrialRecording.js
+++ get-hired-client/src/VirtualInterview/TrialRecording.js
@@ -1,10 +1,9 @@
 import React, { useState, useRef, useEffect} from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faVideo, faVideoSlash, faMicrophone } from '@fortawesome/free-solid-svg-icons';
-
-import "./TrialRecording.css";
+import { faVideo, faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import MicrophoneTest from './MicrophoneTest';
+import VideoInterviewer from './VideoInterviewer';
 
 const TrialRecording = () => {
   const location = useLocation();
@@ -23,10 +22,8 @@ const TrialRecording = () => {
         stopRecording();
       }, 30); // Delay of 1000 milliseconds (adjust as needed)
     };
-  
     performRecording();
   }, []);
-
 
   const startRecording = async () => {
 
@@ -61,15 +58,20 @@ const TrialRecording = () => {
 
   return (
     <div>
-    
+    <h3>Before we begin,
+      please ensure your camera and audio settings are perfectly
+      set.
+      We want you to be seen and heard clearly. Remember, first
+      impressions matter!</h3>
+      <VideoInterviewer width="235" height="420" src="https://drive.google.com/uc?export=download&id=1-OlwazVbzxh4WyQhfcfcXy5Tkyb3s7IK"  />
       <video ref={videoRef} autoPlay muted  /><br></ br>
-
+      
       {!isRecording && <button onClick={startRecording}><FontAwesomeIcon icon={faVideoSlash} /></button>}
       {!isRecording && <button onClick={startRecording}>Start video</button>}
       {isRecording && <button onClick={stopRecording}><FontAwesomeIcon icon={faVideo} /></button>}
       {isRecording&& <button onClick={stopRecording}>Stop video</button>}
       <MicrophoneTest/>
-      <button onClick={handleClick}>Start the interview</button>
+      <button onClick={handleClick}>next</button>
 
     </div>
   );
