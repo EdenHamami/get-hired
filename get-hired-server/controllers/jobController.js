ca9c9a84cb89599aa4da9a937f73a0b1961b094f
hilaninio <vhkv200a@gmail.com>
2023-05-28 16:46:28 +0300

    גקםקמגקמבןקד

--- get-hired-server/controllers/jobController.js
+++ get-hired-server/controllers/jobController.js
@@ -86,7 +86,6 @@ module.exports = function configureServer(app) {
       return vacancyWithoutId;
     });
     const jobsJSON = JSON.stringify(jobs);
-    console.log(jobsJSON)
     res.send(jobsJSON)
   });
 
