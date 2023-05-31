1ab4a66008aa10116a73ebe48741839e5c6b8b93
hilaninio <vhkv200a@gmail.com>
2023-05-31 10:52:44 +0300

    credentials

--- get-hired-server/controllers/openaiController.js
+++ get-hired-server/controllers/openaiController.js
@@ -5,8 +5,8 @@ module.exports = function configureServer(app) {
     console.log('inside')
     const { question } = req.body;
     const configuration = new Configuration({
-      organization: "org-9dFYZrQaxgLw73Ht9RyDsz13",
-      apiKey: "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",
+      organization: 
+      apiKey: 
     });
     const openai = new OpenAIApi(configuration);
     const prompt = "The user is interested in the field of Machine Learning and is proficient in the following programming languages/tools: JavaScript, React.js, HTML/CSS, Node.js. Theyve specified that they are particularly interested in front end. The main purpose of their portfolio is Personal Interest. Could you please provide a light project idea that would be suitable for their portfolio?"
