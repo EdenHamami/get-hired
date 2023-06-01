4158b4ec5c2c68718fd3b2ecaad8f114fac0855c
EdenHamami <edenha1900@gmail.com>
2023-06-01 14:17:15 +0200

    ראיון עבודה וירטולי

--- get-hired-client/src/VirtualInterview/InterviewNavbar.js
+++ get-hired-client/src/VirtualInterview/InterviewNavbar.js
@@ -5,27 +5,28 @@ import { faVideo, faVideoSlash } from '@fortawesome/free-solid-svg-icons';
 import './InterviewNavbar.css';
 import Timer from './Timer';
 import VirtualInterview from "./VirtualInterview"
-import {stopRecording,startRecording} from "./VirtualInterview"
-const InterviewNavbar = ({currentIndex, questionsLength, handleNext, handleAlert, handleRecording, isRecording}) => {
-    return (
-        <Navbar bg="dark" variant="dark" className="justify-content-between interview-navbar">
-            <Nav>
-                <Button variant="danger" className="rounded-pill mr-2" onClick={handleAlert}>End</Button>
-                <Button variant="outline-light" className="rounded-circle" onClick={handleRecording}>
-                    <FontAwesomeIcon icon={isRecording ? faVideo : faVideoSlash} />
-                </Button>
-                <Timer />
-            </Nav>
-            <Nav>
-                {[...Array(questionsLength)].map((_, i) => (
-                    <span key={i} className={currentIndex === i ? 'active-question' : 'inactive-question'}>{i + 1}</span>
-                ))}
-            </Nav>
-            <Nav>
-                <Button variant="outline-light" className="rounded-pill" onClick={handleNext}>Next</Button>
-            </Nav>
-        </Navbar>
-    );
+import { stopRecording, startRecording } from "./VirtualInterview"
+
+const InterviewNavbar = ({ currentIndex, questionsLength, handleNext, handleAlert, handleRecording, isRecording }) => {
+  return (
+    <Navbar bg="dark" variant="dark" className="justify-content-between interview-navbar">
+      <Nav>
+        <Button variant="danger" className="rounded-pill mr-2" onClick={handleAlert}>End</Button>
+        <Button variant="outline-light" className="rounded-circle" onClick={handleRecording}>
+          <FontAwesomeIcon icon={isRecording ? faVideo : faVideoSlash} />
+        </Button>
+        <Timer />
+      </Nav>
+      <Nav className="question-indicators">
+        {[...Array(questionsLength)].map((_, i) => (
+          <span key={i} className={currentIndex === i ? 'active-question' : 'inactive-question'}>{i + 1}</span>
+        ))}
+      </Nav>
+      <Nav>
+        <Button variant="outline-light" className="rounded-pill next-button" onClick={handleNext}>Next</Button>
+      </Nav>
+    </Navbar>
+  );
 };
 
 export default InterviewNavbar;
