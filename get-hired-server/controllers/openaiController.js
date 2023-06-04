330e37c749995d3dafcc74d34b0ae181df383d33
hilaninio <vhkv200a@gmail.com>
2023-06-04 13:14:11 +0300

    gpt answer

--- get-hired-server/controllers/openaiController.js
+++ get-hired-server/controllers/openaiController.js
@@ -8,6 +8,7 @@ module.exports = function configureServer(app) {
     const configuration = new Configuration({
       organization: "",
       apiKey: "",
+
     });
     const openai = new OpenAIApi(configuration);
     const prompt = "The user is interested in the field of Machine Learning and is proficient in the following programming languages/tools: JavaScript, React.js, HTML/CSS, Node.js. Theyve specified that they are particularly interested in front end. The main purpose of their portfolio is Personal Interest. Could you please provide a light project idea that would be suitable for their portfolio?"
@@ -15,9 +16,9 @@ module.exports = function configureServer(app) {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: question }],
-      max_tokens: 64,
+      max_tokens: 1000,
     });
-    console.log(completion.data.choices[0].message);
+    console.log(completion.data.choices[0]);
     return res.status(200).json({ message: completion.data.choices[0].message });
 
   });
